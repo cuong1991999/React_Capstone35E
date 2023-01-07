2cec762f3365c71bf25985b37a21807fef51c72c
cuong1999 <chicuong1999@gmail.com>
2023-01-07 20:58:06 +0700

    up

--- src/components/HeaderHome/HeaderHome.jsx
+++ src/components/HeaderHome/HeaderHome.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { removeStore, sl, TOKEN, USER_LOGIN } from '../../util/config';
+
 const HeaderHome = () => {
   const { arrStore } = useSelector((state) => state.productReducer);
   // render quantity len gio hang khi nguoi dung add to cart
@@ -11,6 +13,49 @@ const HeaderHome = () => {
     }, 0);
     return arr;
   };
+  const { profile } = useSelector(state => state.userReducer);
+  const renderLogin = () => {
+    if (profile) {
+        return <>
+            <NavLink  style={{
+                fontSize: '16px',
+                fontWeight: '300',
+                color: 'rgba(0, 0, 0, 0.7)', lineHeight: '24.2px',
+                padding:'0.5rem 1rem'
+            }} to='/profile'>
+                <span style={{ color: 'white', fontSize: '20px' }}>Hello ! {profile?.email}</span>
+            </NavLink>
+            <span style={{ cursor: 'pointer', color: 'white', fontWeight: '500' }} onClick={() => {
+                removeStore(TOKEN);
+                removeStore(USER_LOGIN);
+                // chuyen huong dang trang f5x
+                window.location.href = '/login';
+            }}>Đăng xuất</span>
+        </>
+    }
+    return <>
+     <NavLink
+              to="/login"
+              className={({ isActive }) =>
+                isActive
+                  ? "header-user__item user__actived "
+                  : "header-user__item"
+              }
+            >
+              Login
+            </NavLink>
+            <NavLink
+              to="/register"
+              className={({ isActive }) =>
+                isActive
+                  ? "header-user__item user__actived "
+                  : "header-user__item"
+              }
+            >
+              Register
+            </NavLink> 
+    </>
+}
   return (
     <header>
       <div className="header py-2">
@@ -42,7 +87,8 @@ const HeaderHome = () => {
 
               <span>({renderQuantity() > 0 ? renderQuantity() : 0}) </span>
             </NavLink>
-            <NavLink
+            {renderLogin()}
+            {/* <NavLink
               to="/login"
               className={({ isActive }) =>
                 isActive
@@ -61,7 +107,7 @@ const HeaderHome = () => {
               }
             >
               Register
-            </NavLink>
+            </NavLink> */}
           </div>
         </div>
       </div>
--- src/templates/HomeTemplate.jsx
+++ src/templates/HomeTemplate.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
 import FooterHome from "../components/FooterHome/FooterHome";
 import HeaderHome from "../components/HeaderHome/HeaderHome";
+import { getProfileApi } from "../redux/reducer/userReducer";
 const HomeTemplate = () => {
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(getProfileApi)
+  },[])
   return (
     <>
       <HeaderHome />
